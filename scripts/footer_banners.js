dd2fd0bae0f26ea61eb7c2bce36ad5c6e04ed282
a.leshko <a.leshko@bsuir.by>
2023-07-06 17:30:04 +0300

    footer banners script done

--- scripts/footer_banners.js
+++ scripts/footer_banners.js
@@ -1,16 +1,82 @@
 const footerBanners = document.querySelector('.footer__banners');
 const footerBannersContent = document.querySelector('.footer__banners_content');
-footerBannersContent.innerHTML += footerBannersContent.innerHTML + footerBannersContent.innerHTML + footerBannersContent.innerHTML + footerBannersContent.innerHTML;
 const scrollStep = 0.3;
+footerBannersContent.innerHTML += footerBannersContent.innerHTML + footerBannersContent.innerHTML + footerBannersContent.innerHTML + footerBannersContent.innerHTML;
+footerBanners.scrollLeft = 3 * footerBannersContent.offsetWidth / 5;
+let scrollPos = 2 * footerBannersContent.offsetWidth / 5;
 let startScrollPos = 0;
-let scrollPos = footerBannersContent.offsetWidth / 5;
 let startMouseX = 0;
 let deltaX = 0;
-let isDragging = false;
 let isMouseOver = false;
-function scrollLoop() {
-    if (!isMouseOver && !isDragging) {
-        scrollPos += scrollStep;
+let isDragging = false;
+
+if ('ontouchstart' in window) {
+
+    function scrollLoop() {
+        if (!isDragging) {
+            scrollPos += scrollStep;
+            if (scrollPos < 1 * footerBannersContent.offsetWidth / 5) {
+                scrollPos += footerBannersContent.offsetWidth / 5;
+            }
+            if (scrollPos > 3 * footerBannersContent.offsetWidth / 5) {
+                scrollPos -= footerBannersContent.offsetWidth / 5;
+            }
+            footerBanners.scrollLeft = scrollPos;
+        }
+        requestAnimationFrame(scrollLoop);
+    }
+    scrollLoop();
+    footerBanners.addEventListener('touchstart', (e) => {
+        isDragging = true;
+        startMouseX = e.touches[0].clientX;
+        startScrollPos = scrollPos;
+        deltaX = 0;
+    })
+    footerBanners.addEventListener('touchmove', (e) => {
+        isDragging = true;
+        deltaX = e.touches[0].clientX - startMouseX;
+        scrollPos = startScrollPos - deltaX;
+        footerBanners.scrollLeft = scrollPos;
+    })
+    footerBanners.addEventListener('touchend', () => {
+        isDragging = false;
+    })
+
+} else {
+
+    function scrollLoop() {
+        if (!isMouseOver && !isDragging) {
+            scrollPos += scrollStep;
+            if (scrollPos < 1 * footerBannersContent.offsetWidth / 5) {
+                scrollPos += footerBannersContent.offsetWidth / 5;
+            }
+            if (scrollPos > 3 * footerBannersContent.offsetWidth / 5) {
+                scrollPos -= footerBannersContent.offsetWidth / 5;
+            }
+            footerBanners.scrollLeft = scrollPos;
+        }
+        requestAnimationFrame(scrollLoop);
+    }
+    scrollLoop();
+    footerBanners.addEventListener('mouseenter', () => {
+        isMouseOver = true;
+    });
+    footerBanners.addEventListener('mousedown', (e) => {
+        e.preventDefault();
+        isDragging = true;
+        startMouseX = e.clientX;
+        startScrollPos = scrollPos;
+        deltaX = 0;
+    });
+    footerBanners.addEventListener('mousemove', (e) => {
+        if (isDragging) {
+            deltaX = e.clientX - startMouseX;
+            scrollPos = startScrollPos - deltaX;
+            footerBanners.scrollLeft = scrollPos;
+        }
+    });
+    footerBanners.addEventListener('mouseup', () => {
+        isDragging = false;
         if (scrollPos < 2 * footerBannersContent.offsetWidth / 5) {
             scrollPos += footerBannersContent.offsetWidth / 5;
         }
@@ -18,46 +84,19 @@ function scrollLoop() {
             scrollPos -= footerBannersContent.offsetWidth / 5;
         }
         footerBanners.scrollLeft = scrollPos;
+    });
+    footerBanners.addEventListener('mouseleave', () => {
+        isMouseOver = false;
+        isDragging = false;
+    });
+
+    const footerBannerCollection = document.querySelectorAll('.footer__banner');
+    for (let banner of footerBannerCollection) {
+        banner.addEventListener('click', (e) => {
+            if (deltaX !== 0) {
+                e.preventDefault();
+            }
+        })
     }
-    requestAnimationFrame(scrollLoop);
-}
-scrollLoop();
-footerBanners.addEventListener('mouseenter', () => {
-    isMouseOver = true;
-});
-footerBanners.addEventListener('mouseleave', () => {
-    isMouseOver = false;
-    isDragging = false;
-});
-footerBanners.addEventListener('mousedown', (e) => {
-    e.preventDefault();
-    isDragging = true;
-    startMouseX = e.clientX;
-    startScrollPos = scrollPos;
-    deltaX = 0;
-});
-footerBanners.addEventListener('mousemove', (e) => {
-    if (isDragging) {
-        deltaX = e.clientX - startMouseX;
-        scrollPos = startScrollPos - deltaX;
-        footerBanners.scrollLeft = scrollPos;
-    }
-});
-footerBanners.addEventListener('mouseup', (e) => {
-    isDragging = false;
-    if (scrollPos < 2 * footerBannersContent.offsetWidth / 5) {
-        scrollPos += footerBannersContent.offsetWidth / 5;
-    }
-    if (scrollPos > 3 * footerBannersContent.offsetWidth / 5) {
-        scrollPos -= footerBannersContent.offsetWidth / 5;
-    }
-    footerBanners.scrollLeft = scrollPos;
-});
-const footerBannerCollection = document.querySelectorAll('.footer__banner');
-for (let banner of footerBannerCollection) {
-    banner.addEventListener('click', (e) => {
-        if (deltaX !== 0) {
-            e.preventDefault();
-        }
-    })
+
 }
